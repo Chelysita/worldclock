9525cd17e38f852e495ef4ccf12ef5c6fc818099
Chelysita <ara.medinago@gmail.com>
2025-04-22 08:45:16 -0400

    JS with 2 cities was added to the project

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,21 @@
-alert("Hola");
+function updateTime() {
+  let mexicoCityElement = document.querySelector("#mexico-city");
+  let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
+  let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
+  let mexicoCityTime = moment().tz("America/Mexico_City");
+  mexicoCityDateElement.innerHTML = mexicoCityTime.format("MMMM Do YYYY");
+  mexicoCityTimeElement.innerHTML = mexicoCityTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let bostonElement = document.querySelector("#boston");
+  let bostonDateElement = bostonElement.querySelector(".date");
+  let bostonTimeElement = bostonElement.querySelector(".time");
+  let bostonTime = moment().tz("America/New_York");
+  bostonDateElement.innerHTML = bostonTime.format("MMMM Do YYYY");
+  bostonTimeElement.innerHTML = bostonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
